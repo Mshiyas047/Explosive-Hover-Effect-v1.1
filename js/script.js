00aca7637c08ccf6114c141453438d5b817c07ee
Muhammed Shiyas <92246435+Mshiyas047@users.noreply.github.com>
2023-02-21 21:53:05 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const enhance = id => {
+    const element =document.getElementById(id),
+    text = element.innerText.split("");
+
+    element.innerText = "";
+
+    text.forEach(letter => {
+        const span = document.createElement("span");
+
+        span.className = "letter";
+
+        span.innerText = letter;
+
+        element.appendChild(span);
+    });
+}
+
+enhance("link");
+enhance("link2");
+enhance("link3");
+
+
+
